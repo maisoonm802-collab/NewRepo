6375cfd4f45c2ef7bbcaba1e12d941bd5fb981a4
maisoon <HP@LAPTOP-F7BI9PDB>
2026-07-06 13:48:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double difference = Math.Abs(num1 - num2);
+
+            Console.WriteLine("Positive Difference: " + difference);
+        }
+    }
+
+}
+        
+    
+        
+    
+    
+ 
+       
+    
+

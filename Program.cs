1071b012ef6fbd2fd480a1ad2049d2e887fe44a0
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-07 10:02:57 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -13,16 +13,35 @@
             double difference = Math.Abs(num1 - num2);
 
             Console.WriteLine("Positive Difference: " + difference);
-        }
-    }
+///Task2\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+Console.WriteLine("\n--- Task 2 ---");
+ Console.Write("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
 
-}
-        
-    
-        
-    
-    
- 
-       
-    
+            double square = Math.Pow(number, 2);
+            double root = Math.Sqrt(number);
+
+            Console.WriteLine("Square: " + square);
+            Console.WriteLine("Square Root: " + root);
+            ///Task3\\\\\\\\\\\\
+            Console.WriteLine("\n--- Task 3 ---");
+            Console.Write("Enter your full name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Uppercase: " + name.ToUpper());
+            Console.WriteLine("Lowercase: " + name.ToLower());
+            Console.WriteLine("Character Count: " + name.Length);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
